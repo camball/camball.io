56898ace5ee2dc13bee98b33d971c07bdeb6e001
camball <67163069+camball@users.noreply.github.com>
2024-06-27 02:50:43 -0700

    feat(tailwind): initial integration
    closes #1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
-<h1 id="hello-msg">hi!</h1>
-<p>
-	<code>camball.io</code>
-	<span>is still under development.</span>
-</p>
-<p>check back soon :)</p>
+<div class="flex h-screen bg-white dark:bg-slate-800">
+	<div class="m-auto space-y-1.5 text-center">
+		<h1 class="text-3xl font-bold text-slate-900 dark:text-white">hi</h1>
+		<p class="text-slate-500 dark:text-slate-400">
+			<code>camball.io</code>
+			<span>is still under development.</span>
+		</p>
+		<p class="text-slate-500 dark:text-slate-400">check back soon :)</p>
+	</div>
+</div>
