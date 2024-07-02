c30469e96e86591c093a1e285d7cd14bfe26b189
camball <67163069+camball@users.noreply.github.com>
2024-07-02 01:31:37 -0700

    feat(fonts): add crimson pro
    closes #3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <div class="flex h-screen bg-white dark:bg-slate-800">
 	<div class="m-auto space-y-1.5 text-center">
-		<h1 class="text-3xl font-bold text-slate-900 dark:text-white">hi</h1>
-		<p class="text-slate-500 dark:text-slate-400">
-			<code>camball.io</code>
-			<span>is still under development.</span>
-		</p>
+		<h1 class="text-3xl font-bold text-slate-900 dark:text-white">camball.io</h1>
+		<p class="text-slate-500 dark:text-slate-400">is still under development.</p>
 		<p class="text-slate-500 dark:text-slate-400">check back soon :)</p>
 	</div>
 </div>
