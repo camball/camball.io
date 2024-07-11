6c8a9c4e9ffe14450c3c6e0156b4f9d669a86cc1
camball <67163069+camball@users.noreply.github.com>
2024-07-10 23:15:32 -0700

    feat(under-dev-page): major improvement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
-<div class="flex h-screen bg-white dark:bg-slate-800">
-	<div class="m-auto space-y-1.5 text-center">
-		<h1 class="text-3xl font-bold text-slate-900 dark:text-white">camball.io</h1>
-		<p class="text-slate-500 dark:text-slate-400">is still under development.</p>
-		<p class="text-slate-500 dark:text-slate-400">check back soon :)</p>
+<script>
+	import Clock from '../components/Clock.svelte';
+</script>
+
+<div class="flex h-screen bg-neutral-50 dark:bg-neutral-900">
+	<div
+		class="absolute m-auto ml-3 space-y-0.5 border-b p-3 text-neutral-700 sm:ml-5 sm:text-right sm:text-neutral-900 sm:dark:text-neutral-50"
+	>
+		<h1 class="mt-20 text-7xl font-bold tracking-tighter sm:text-9xl">camball.io</h1>
+		<div class="pl-1 text-lg sm:dark:text-neutral-400">
+			<p>is still under development.</p>
+			<p>check back soon :)</p>
+			<Clock />
+		</div>
 	</div>
+	<img
+		src="sitting-on-couch.jpeg"
+		alt="Cameron, sitting on a couch in a tall, well-lit room, wearing a ball cap and crossing one leg over the other, looking out a large window."
+		class="object-cover sm:ml-auto sm:py-4 sm:pr-3"
+	/>
 </div>
