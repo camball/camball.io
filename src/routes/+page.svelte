96ac37930c0c3d701f0052450a9678e0cfcff2c3
camball <67163069+camball@users.noreply.github.com>
2024-07-12 02:52:00 -0700

    feat(under-dev-page): binary display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
 	import Clock from '../components/Clock.svelte';
+	import BinaryTextOverlay from '../components/BinaryTextOverlay.svelte';
 </script>
 
 <div class="flex h-screen bg-neutral-50 dark:bg-neutral-900">
+	<div class="absolute sm:mt-20 sm:ml-20">
+		<BinaryTextOverlay text={'discipline'} />
+	</div>
 	<div
-		class="absolute m-auto ml-3 space-y-0.5 border-b p-3 text-neutral-700 sm:ml-5 sm:text-right sm:text-neutral-900 sm:dark:text-neutral-50"
+		class="text-shadow absolute m-auto ml-5 space-y-0.5 border-b p-3 pl-0 text-neutral-700 sm:ml-5 sm:text-right sm:text-neutral-900 sm:dark:text-neutral-50"
 	>
 		<h1 class="mt-20 text-7xl font-bold tracking-tighter sm:text-9xl">camball.io</h1>
 		<div class="pl-1 text-lg sm:dark:text-neutral-400">
