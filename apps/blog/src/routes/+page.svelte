16d4c5d19501ba9b2818541def5ca33f7f609b01
camball <67163069+camball@users.noreply.github.com>
2024-07-15 04:08:01 -0700

    feat(repo): monorepo migration for subdomains

--- apps/blog/src/routes/+page.svelte
+++ apps/blog/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 	<div
 		class="text-shadow absolute m-auto ml-5 space-y-0.5 border-b p-3 pl-0 text-neutral-700 sm:ml-5 sm:text-right sm:text-neutral-900 sm:dark:text-neutral-50"
 	>
-		<h1 class="mt-20 text-7xl font-bold tracking-tighter sm:text-9xl">camball.io</h1>
+		<h1 class="mt-20 pb-1 text-6xl font-bold tracking-tighter sm:text-9xl">blog.camball.io</h1>
 		<div class="pl-1 text-lg sm:dark:text-neutral-400">
 			<p>is still under development.</p>
 			<p>check back soon :)</p>
@@ -18,7 +18,7 @@
 		</div>
 	</div>
 	<img
-		src="sitting-on-couch.jpeg"
+		src="$lib/assets/sitting-on-couch.jpeg"
 		alt="Cameron, sitting on a couch in a tall, well-lit room, wearing a ball cap and crossing one leg over the other, looking out a large window."
 		class="object-cover sm:ml-auto sm:py-4 sm:pr-3"
 	/>
