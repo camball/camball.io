7db36794fa48423ffe98f041051df947de7a1278
camball <67163069+camball@users.noreply.github.com>
2024-08-03 20:03:36 -0700

    feat: social links and shadcn-svelte integration

---
+++ apps/lib/components/ui/index.ts
@@ -0,0 +1,4 @@
+import { Badge } from '$lib/components/ui/badge';
+import { Button } from '$lib/components/ui/button';
+
+export { Badge, Button };
--- apps/main/src/routes/+page.svelte
+++ apps/main/src/routes/+page.svelte
@@ -1,10 +1,13 @@
-<script>
+<script lang="ts">
 	import Clock from '../components/Clock.svelte';
 	import BinaryTextOverlay from '../components/BinaryTextOverlay.svelte';
+	import { Badge, Button } from '$lib/components/ui';
+	import { Moon, Sun } from 'lucide-svelte/icons';
+	import { toggleMode, mode } from 'mode-watcher';
 </script>
 
 <div class="flex h-screen bg-neutral-50 dark:bg-neutral-900">
-	<div class="absolute sm:mt-20 sm:ml-20">
+	<div class="absolute sm:ml-20 sm:mt-20">
 		<BinaryTextOverlay text={'discipline'} />
 	</div>
 	<div
@@ -22,4 +25,40 @@
 		alt="Cameron, sitting on a couch in a tall, well-lit room, wearing a ball cap and crossing one leg over the other, looking out a large window."
 		class="object-cover sm:ml-auto sm:py-4 sm:pr-3"
 	/>
+	<div class="fixed bottom-3 left-3 flex flex-col space-y-1.5">
+		<Button on:click={toggleMode} variant="outline" size="icon">
+			<Sun
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+		<Badge variant="secondary">
+			<div class="my-1 flex space-x-2">
+				<a href="https://github.com/camball">
+					{#if $mode === 'dark'}
+						<img src="$lib/assets/github-logo-light.png" alt="GitHub logo" width="25px" />
+					{:else}
+						<img src="$lib/assets/github-logo-dark.png" alt="GitHub logo" width="25px" />
+					{/if}
+				</a>
+				<a href="https://www.linkedin.com/in/camballl">
+					{#if $mode === 'dark'}
+						<img src="$lib/assets/linkedin-logo-light.png" alt="LinkedIn logo" width="25px" />
+					{:else}
+						<img src="$lib/assets/linkedin-logo-dark.png" alt="LinkedIn logo" width="25px" />
+					{/if}
+				</a>
+				<a href="https://www.instagram.com/camballl">
+					{#if $mode === 'dark'}
+						<img src="$lib/assets/instagram-logo-light.png" alt="Instagram logo" width="25px" />
+					{:else}
+						<img src="$lib/assets/instagram-logo-dark.png" alt="Instagram logo" width="25px" />
+					{/if}
+				</a>
+			</div>
+		</Badge>
+	</div>
 </div>
