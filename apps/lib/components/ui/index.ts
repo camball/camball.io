7301a55ef72cff8a1959bf885fbe6f2f887f9213
camball <67163069+camball@users.noreply.github.com>
2024-08-28 00:48:38 -0700

    feat(blog): landing page scroll area

--- apps/lib/components/ui/index.ts
+++ apps/lib/components/ui/index.ts
@@ -2,6 +2,7 @@ import { Badge } from '$lib/components/ui/badge';
 import { Button } from '$lib/components/ui/button';
 import * as Card from '$lib/components/ui/card';
 import * as Drawer from '$lib/components/ui/drawer';
+import { ScrollArea } from '$lib/components/ui/scroll-area';
 import { Separator } from '$lib/components/ui/separator';
 
-export { Badge, Button, Card, Drawer, Separator };
+export { Badge, Button, Card, Drawer, ScrollArea, Separator };
