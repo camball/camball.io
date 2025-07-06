5745fc778ad7679d8cec0b0739ceb8a3cb5a289c
camball <67163069+camball@users.noreply.github.com>
2025-07-05 17:59:37 -0700

    meta(lib): fix format/lint; simplify package scripts

--- apps/lib/components/ui/carousel/carousel-item.svelte
+++ apps/lib/components/ui/carousel/carousel-item.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-	import type { HTMLAttributes } from 'svelte/elements';
-	import { getEmblaContext } from './context.js';
-	import { cn } from '$lib/utils.js';
+    import { cn } from "$lib/utils.js";
 
-	type $$Props = HTMLAttributes<HTMLDivElement>;
-	let className: string | undefined | null = undefined;
-	export { className as class };
-
-	const { orientation } = getEmblaContext('<Carousel.Item/>');
+    let className: string | undefined | null = undefined;
+    export { className as class };
 </script>
 
 <div
-	role="group"
-	aria-roledescription="slide"
-	class={cn('min-w-0 shrink-0 grow-0 basis-full', className)}
-	data-embla-slide=""
-	{...$$restProps}
+    role="group"
+    aria-roledescription="slide"
+    class={cn("min-w-0 shrink-0 grow-0 basis-full", className)}
+    data-embla-slide=""
+    {...$$restProps}
 >
-	<slot />
+    <slot />
 </div>
