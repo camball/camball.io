fafa2ca0e2614d925fb32448c3159da352f4750b
camball <67163069+camball@users.noreply.github.com>
2025-08-24 00:58:07 -0700

    feat(blog): unify header in articles with rest of site
    closes #53

--- apps/blog/src/lib/header.svelte
+++ apps/blog/src/lib/header.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import ThemeToggle from "$lib/components/ThemeToggle.svelte";
+    import { Menu } from "@lucide/svelte";
     import { mode } from "mode-watcher";
+    import { Drawer } from "$lib/components/ui";
+    import { cn } from "$lib/utils";
+    import { buttonVariants } from "$lib/components/ui/button";
+    import { siteLinksEntries } from "$lib/shared/site-links";
+    import { HeaderDrawerContent, Socials, ThemeToggle } from "$lib/components";
 
     let previousScrollPosition = 0;
     let header: HTMLDivElement | undefined = $state();
@@ -36,15 +41,45 @@
 
 <div
     bind:this={header}
-    class="top-0 left-0 z-50 flex w-full items-center justify-between bg-stone-100 px-3 py-2 dark:bg-stone-900"
+    class="top-0 left-0 z-50 flex w-full items-center justify-between bg-stone-100 px-3 py-2 sm:px-4 dark:bg-stone-900"
 >
-    <a href="/">
-        {#if mode.current === "dark"}
-            <img src="$lib/assets/cb_logo_light.svg" alt="Logo Light" class="h-11" />
-        {:else}
-            <img src="$lib/assets/cb_logo_dark.svg" alt="Logo Dark" class="h-11" />
-        {/if}
-    </a>
-    <ThemeToggle variant="outline" />
+    <div class="flex items-center space-x-6">
+        <a href="/">
+            {#if mode.current === "dark"}
+                <img src="$lib/assets/cb_logo_light.svg" alt="Logo Light" class="h-11" />
+            {:else}
+                <img src="$lib/assets/cb_logo_dark.svg" alt="Logo Dark" class="h-11" />
+            {/if}
+        </a>
+        <div
+            class="*:hover:text-shadow-6xl hidden items-center space-x-6 *:transition-all *:ease-in-out *:hover:transition-all *:hover:ease-in-out sm:flex"
+        >
+            {#each siteLinksEntries() as [title, link] (link)}
+                <p class="text-xl font-semibold">
+                    <a href={link}>{title}</a>
+                </p>
+            {/each}
+        </div>
+    </div>
+    <div class="hidden items-center space-x-2 px-2 sm:flex">
+        <ThemeToggle variant="ghost" />
+        <Socials imageSize="25px" class="space-x-3" />
+    </div>
+
+    <Drawer.Root direction="top">
+        <Drawer.Trigger>
+            {#snippet child({ props })}
+                <div {...props} class={cn(buttonVariants({ variant: "ghost" }), "p-1 sm:hidden")}>
+                    <Menu
+                        color={mode.current === "dark" ? "#EEE" : "#111"}
+                        size="28"
+                        strokeWidth="2.5"
+                        class="m-1"
+                    />
+                </div>
+            {/snippet}
+        </Drawer.Trigger>
+        <HeaderDrawerContent />
+    </Drawer.Root>
 </div>
 <div bind:this={spacer} class="top-0 left-0 hidden h-16 w-full"></div>
---
+++ apps/lib/components/HeaderDrawerContent.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Button, Drawer } from "$lib/components/ui";
+    import { ArrowUp } from "@lucide/svelte";
+    import { Socials } from "$lib/components";
+    import { toggleMode, mode } from "mode-watcher";
+    import { siteLinksEntries } from "$lib/shared/site-links";
+</script>
+
+<Drawer.Content class="flex flex-col items-end gap-5 p-6 font-sans">
+    <Socials imageSize="30px" class="space-x-4" />
+    <Button onclick={toggleMode} variant="outline">
+        {#if mode.current === "dark"}Toggle Light{:else}Toggle Dark{/if}
+    </Button>
+    {#each siteLinksEntries() as [title, link] (link)}
+        <p class="pr-1 text-xl font-semibold">
+            <a href={link}>{title}</a>
+        </p>
+    {/each}
+    <Drawer.Close><ArrowUp size="28px" /></Drawer.Close>
+</Drawer.Content>
