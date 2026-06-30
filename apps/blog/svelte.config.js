0cd7087b0876852a420835480b1fe0a0cdbf11be
Cameron Ball <camball424@gmail.com>
2026-06-29 18:02:07 -0700

    feat(blog): add footnote support (#125)

--- apps/blog/svelte.config.js
+++ apps/blog/svelte.config.js
@@ -4,6 +4,7 @@ import { importAssets } from "svelte-preprocess-import-assets";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
 import rehypeToc from "rehype-toc";
+import remarkFootnotes from "remark-footnotes";
 import remarkGfm from "remark-gfm";
 import remarkCallout from "@r4ai/remark-callout";
 import remarkCalloutFix from "./remark-callout-fix.js";
@@ -12,11 +13,22 @@ import { join } from "node:path";
 
 const rehypeTocOpts = { position: "beforeend" };
 
+// `remark-footnotes` pinned to v2 only, as v3+ uses micromark and
+// won't hook into mdsvex's remark-parse v8.
+// TODO: Move away from mdsvex, as it hasn't had an update for a year+
+// TODO: and is the root of many issues with TOC and callouts.
+const remarkFootnotesOpts = { inlineNotes: true };
+
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexConfig = {
     extension: ".mdx",
     layout: join(import.meta.dirname, "src/layouts/article.svelte"),
-    remarkPlugins: [remarkCalloutFix, remarkCallout, remarkGfm],
+    remarkPlugins: [
+        remarkCalloutFix,
+        remarkCallout,
+        [remarkFootnotes, remarkFootnotesOpts],
+        remarkGfm,
+    ],
     rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, [rehypeToc, rehypeTocOpts]],
 };
 
