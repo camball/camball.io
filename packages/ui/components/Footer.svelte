e909a174853aa60210ceec4f25b26be90007d85d
camball <67163069+camball@users.noreply.github.com>
2026-08-20 12:20:35 -0700

    feat(blog): footer art (#155)

--- packages/ui/components/Footer.svelte
+++ packages/ui/components/Footer.svelte
@@ -3,10 +3,25 @@
     const date = `2022–${new Date().getFullYear()}`;
 </script>
 
-<footer class="space-y-2 bg-stone-100 p-6 sm:px-12 dark:bg-stone-900">
-    <p>{`Copyright © ${date} Cameron Ball. All rights reserved.`}</p>
-    <p>
-        Opinions on this site are my own and are not those of any of my employers, past, present,
-        and/or future.
-    </p>
+<footer class="space-y-6 bg-stone-100 dark:bg-stone-900">
+    <pre class="pt-6 text-center leading-4 tracking-[-0.125em]">
+⠀⣀⣤⡀⠀⠀⢀⣀⠀⢀⡤⣄⡀⢀⣀⣠⣤⣤⣄⣀⠀⠀⠀⢀⣀⣤⣤⣤⣀⣀⠀⣠⡤⡄⠀⢀⡀⠀⠀⢀⣠⣄⡀
+⢸⠋⢠⡽⠀⣴⠋⢩⣿⢈⣡⡽⠿⠿⠛⣩⠴⠒⠒⠺⣷⣻⣷⠿⠒⠒⠢⣭⡙⠻⠿⠿⣤⡁⢼⣫⠙⢷⡀⢸⡧⠈⢳
+⠘⢦⡀⠀⠀⢧⣀⡼⠿⠋⠁⠀⣠⡴⠋⠁⠀⠀⠀⠀⠈⣿⡇⠀⠀⠀⠀⠀⠙⠳⣄⡀⠀⠙⠻⢧⣄⣸⠃⠀⢀⣠⠏
+⠀⠀⠀⠉⠉⠉⠓⠤⠤⠤⠴⠛⠁⠀⠀⠀⠀⠸⣟⣀⡼⣿⢷⣄⣛⡵⠀⠀⠀⠀⠈⠙⠲⠤⠤⠤⠜⠋⠉⠉⠁⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠹⠷⣿⠷⠏⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀</pre>
+    <!-- TODO: Replace these two lines with spinning 3js animation. -->
+    <!-- TODO: For now, desktop- and mobile-specific versions b/c hearts -->
+    <!-- TODO: render slightly different on commonm mobile browsers. -->
+    <p class="px-6 text-right sm:hidden">₊˚⊹ Made with ❤ by Cameron.</p>
+    <p class="hidden px-6 text-right sm:block sm:px-12">₊˚⊹ Made with ♥ by Cameron.</p>
+
+    <div class="space-y-2 px-6 pb-6 sm:px-12">
+        <p>{`Copyright © ${date} Cameron Ball. All rights reserved.`}</p>
+        <p>
+            Opinions on this site are my own and are not those of any of my employers, past,
+            present, and/or future.
+        </p>
+    </div>
 </footer>
